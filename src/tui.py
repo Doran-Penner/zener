aa4a20f0211b1ec881c3183801e94341544cd868
Sam <30577766+Samasaur1@users.noreply.github.com>
2025-01-21 13:55:36 -0800

    tui: adapt to use play_game

--- src/tui.py
+++ src/tui.py
@@ -1,50 +1,51 @@
-from core import Color, Move, MoveResult, Shape, State
+from core import State, Color, Move, Shape
+from play_game import play_game, MoveGetter
 from pprint import pp
 
-game = State()
 
-response, extra_info = ("filler", "")
+def get_getter_for_player(player: Color) -> MoveGetter:
+    def f(board, valid, responding, prev, required_move) -> Move:
+        if required_move is None:
+            required_move = "any"
+        else:
+            required_move = "their " + required_move
+        print(f"It is {player}'s move, who must move {required_move} piece next.")
 
-while response not in [
-    MoveResult.WIN_WHITE,
-    MoveResult.WIN_BLACK,
-    MoveResult.ALREADY_OVER,
-]:
-    game.update_board()
-    player_turn, required_move = game.get_next_move()
-    if required_move is None:
-        required_move = "any"
-    else:
-        required_move = "their " + required_move
-    print(f"It is {player_turn}'s move, who must move {required_move} piece next.")
+        print("Valid moves are:")
+        pp(valid)
 
-    print("The board state is:")
-    pp(game.get_full_board())
-    game.draw_board()
+        print("Input your move request.")
+        in_player = input('player ("black" or "white"): ')
+        in_shape = input('shape (e.g. "wave"): ')
+        in_x = input("x (e.g. 2): ")
+        in_y = input("y (e.g. 1): ")
 
-    print("Valid moves are:")
-    pp(game.get_valid_moves())
+        try:
+            full_request = Move(Color(in_player), Shape(in_shape), int(in_x), int(in_y))
+        except ValueError:
+            print('ERROR: Invalid input type (e.g. "wave" for x)')
+        print(f"Your move: {full_request}")
 
-    print("Input your move request.")
-    in_player = input('player ("black" or "white"): ')
-    in_shape = input('shape (e.g. "wave"): ')
-    in_x = input("x (e.g. 2): ")
-    in_y = input("y (e.g. 1): ")
+        return full_request
 
-    try:
-        full_request = Move(Color(in_player), Shape(in_shape), int(in_x), int(in_y))
-    except ValueError:
-        print('ERROR: Invalid input type (e.g. "wave" for x)')
-    print(f"Your move: {full_request}")
-    response, extra_info = game.try_move(full_request)
+    return f
 
-    print()
-    print(f"{response}: {extra_info}")
+
+def end_of_turn():
     end_input = input("[Press any key to continue]")
     # sneaky debug console
     if end_input == "debug":
         breakpoint()
     print()
 
-print("Game is over!")
-print(f"Winner: {game.get_who_won()}")
+
+winner = play_game(
+    get_white_move=get_getter_for_player(Color.WHITE),
+    get_black_move=get_getter_for_player(Color.BLACK),
+    verbose=True,
+    sleep_time=0.25,
+    draw_over=True,
+    end_of_turn_hook=end_of_turn,
+)
+
+print(f"Game is over! Winner: {winner}")
