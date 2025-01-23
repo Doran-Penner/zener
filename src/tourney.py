31819a8f6848082b9e367d3f77478493ef4838ba
Doran Penner <doranaviv@gmail.com>
2025-01-22 19:14:02 -0800

    tourney: make executable, format

--- src/tourney.py
+++ src/tourney.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 from core import Color
 from move_getters import get_from_bot
@@ -9,7 +11,7 @@ PLAYER_COUNT = len(PLAYERS)
 print("You've passed the following bots as players:")
 print(PLAYERS)
 
-winners: dict[str, dict[str, Color]] = { bot: {} for bot in PLAYERS}
+winners: dict[str, dict[str, Color]] = {bot: {} for bot in PLAYERS}
 stats: dict[str, tuple[float, float, float]] = {}
 
 for white_bot in PLAYERS:
@@ -22,8 +24,12 @@ for white_bot in PLAYERS:
 
 
 for bot in PLAYERS:
-    white_wins = sum(1 if winners[bot][other] == Color.WHITE else 0 for other in winners[bot])
-    black_wins = sum(1 if winners[other][bot] == Color.BLACK else 0 for other in winners[bot])
+    white_wins = sum(
+        1 if winners[bot][other] == Color.WHITE else 0 for other in winners[bot]
+    )
+    black_wins = sum(
+        1 if winners[other][bot] == Color.BLACK else 0 for other in winners[bot]
+    )
     white_win_rate = white_wins / (PLAYER_COUNT - 1)
     black_win_rate = black_wins / (PLAYER_COUNT - 1)
     win_rate = (white_wins + black_wins) / ((PLAYER_COUNT * 2) - 2)
@@ -32,12 +38,12 @@ for bot in PLAYERS:
 print("\033[2J\033[H")  # clear screen, return to terminal position 0,0
 print("OVERALL:")
 for bot in sorted(stats, key=lambda bot: stats[bot][2], reverse=True):
-      print(f"- {stats[bot][2]:.0%} ({bot})")
+    print(f"- {stats[bot][2]:.0%} ({bot})")
 
 print("WHEN GOING FIRST:")
 for bot in sorted(stats, key=lambda bot: stats[bot][0], reverse=True):
-      print(f"- {stats[bot][0]:.0%} ({bot})")
+    print(f"- {stats[bot][0]:.0%} ({bot})")
 
 print("WHEN GOING SECOND:")
 for bot in sorted(stats, key=lambda bot: stats[bot][1], reverse=True):
-      print(f"- {stats[bot][1]:.0%} ({bot})")
+    print(f"- {stats[bot][1]:.0%} ({bot})")
